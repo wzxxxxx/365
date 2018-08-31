6d6cc7e6ce3f0a1807dbeb03465daddc58e5af58
wzxxxxx <wuzhixue23@gmail.com>
2018-08-31 22:00:00 +0800

    autocommit

--- js/xiachenshi.js
+++ js/xiachenshi.js
@@ -6,7 +6,7 @@ function tiaozhengTOP(){
 	var iszhichi = plus.navigator.isImmersedStatusbar();
 	var topoffset;
 	if(iszhichi) {
-		topoffset = (Math.round(plus.navigator.getStatusbarHeight()) + 35) + 'px';
+		topoffset = (Math.round(plus.navigator.getStatusbarHeight()) + 45) + 'px';
 		document.querySelector("header").style.height=topoffset;
 		document.querySelector("header").style.paddingTop="20px";
 		document.querySelector(".mui-bar-nav~.mui-content").style.paddingTop=topoffset;
